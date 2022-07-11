596c62e8ac61383124ba459c12416c5dcaf57037
Andrey Skiba <skiba_andrey@mail.ru>
2022-07-11 15:48:02 +0300

    Update bash_oneliners.sh

--- bash_oneliners.sh
+++ bash_oneliners.sh
@@ -4,7 +4,7 @@
 echo "$tail -n 10 log.txt" > log.txt
 
 # find n biggest files or dirs
-du -a . | sort -n -r | head -n 20
+du 2>/dev/null -a . | sort -n -r | head -n 20
 
 # find duplicates
 find . ! -empty -type f -exec md5sum {} + | sort | uniq -w32 -dD
